d551e9b6eadc9f02499789d3aa50ab9b12731536
Jaison R <joshuasix@gmail.com>
2017-05-08 09:35:56 -0400

    Add project files.

---
+++ getToken/Program.cs
@@ -0,0 +1,25 @@
+﻿using Box.V2.Config;
+using Box.V2.JWTAuth;
+using BoxDelete2;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BoxConfigJSON oCfg = JsonConvert.DeserializeObject<BoxConfigJSON>(File.ReadAllText("box_app_config.json"));
+            var boxConfig = new BoxConfig(oCfg.boxAppSettings.clientID, oCfg.boxAppSettings.clientSecret, oCfg.enterpriseID, oCfg.boxAppSettings.appAuth.privateKey, oCfg.boxAppSettings.appAuth.passphrase, oCfg.boxAppSettings.appAuth.publicKeyID);
+            var boxJWT = new BoxJWTAuth(boxConfig);
+            var adminToken = boxJWT.AdminToken(); //valid for 60 minutes so should be cached and re-used  
+            File.WriteAllText("boxtoken.txt", adminToken);
+        }
+    }
+}
---
+++ getToken/box_app_config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxDelete2
+{
+    public class AppAuth
+    {
+        public string publicKeyID { get; set; }
+        public string privateKey { get; set; }
+        public string passphrase { get; set; }
+    }
+
+    public class BoxAppSettings
+    {
+        public string clientID { get; set; }
+        public string clientSecret { get; set; }
+        public AppAuth appAuth { get; set; }
+    }
+
+    public class BoxConfigJSON
+    {
+        public BoxAppSettings boxAppSettings { get; set; }
+        public string enterpriseID { get; set; }
+    }
+}
